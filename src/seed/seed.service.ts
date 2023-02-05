a9072f2d2a18fa06d4dd9998560884005c5fabae
holgadoM <holgado.m@gmail.com>
2023-02-05 19:04:32 -0300

    httpAdapter and filter findAll

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import axios,{ AxiosInstance } from 'axios';
 import { Model } from 'mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
 
-  private readonly axis:AxiosInstance = axios;
-
+  
   constructor(
-
+    
+    private readonly axios:AxiosAdapter,
     @InjectModel( Pokemon.name )
     private readonly pokemonModel: Model<Pokemon>
   ){
@@ -19,8 +19,8 @@ export class SeedService {
 
   async executeSeed() {
     await this.pokemonModel.deleteMany();
-    const {data} = await this.axis.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650&offset=0');
-    var listPokemons = []
+    const data = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=600&offset=0');
+    var listPokemons = [];
     data.results.forEach(({name, url})=>{
       var segments = url.split('/');
       const id = +segments[segments.length-2];
